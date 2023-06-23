a2f7ca24d4a2423ecb592c220e96f08fa27d3c1e
Arjun <marjunram95@gmail.com>
2023-06-23 15:20:16 +0530

    second commit

---
+++ ButtonExample.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+public class ButtonExample extends Component {
+	public static void main(String[] args) {
+		Frame f = new Frame("Button Example");
+		Button b = new Button("Click me");
+		b.setBounds(150,50,50,80);
+		f.add(b);
+		f.setSize(600,600);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+}
